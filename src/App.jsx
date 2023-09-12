bcdd6167c22318d2990f49082e6a7bdd76982b75
siyam79 <siyamahmed3827@gmail.com>
2023-09-12 20:34:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Blog from './componnets/Blogs/Blog'
+import Header from './componnets/Header/Header'
+
+function App() {
+
+
+  return (
+    <>
+     
+     <Header></Header>
+     <Blog></Blog>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/componnets/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+    return (
+        <div>
+             <h1 className=' text-3xl font-bold py-8 '>Knowledge Cafe </h1>
+           
+        </div>
+    );
+};
+
+export default Header;
